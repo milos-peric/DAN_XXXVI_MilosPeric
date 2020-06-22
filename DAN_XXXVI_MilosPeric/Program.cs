fedc308aeafde4535efff7d76688a1155d18062b
milos.peric <steamestt@gmail.com>
2020-06-22 15:47:28 +0200

    +Add generate numbers +Add create and assign matrix

--- DAN_XXXVI_MilosPeric/Program.cs
+++ DAN_XXXVI_MilosPeric/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXVI_MilosPeric
@@ -10,6 +11,15 @@ namespace DAN_XXXVI_MilosPeric
     {
         static void Main(string[] args)
         {
+            MatrixFunctions treads = new MatrixFunctions();
+            Thread t1 = new Thread(new ThreadStart(treads.GenerateMatrix));
+            t1.Name = "Thread 1";
+            Thread t2 = new Thread(new ThreadStart(treads.GenerateRandomNumbers));
+            t2.Name = "Thread 2";
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
         }
     }
 }
