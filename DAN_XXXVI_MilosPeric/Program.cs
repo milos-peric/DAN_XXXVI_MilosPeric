197dedf72917e8dbf43f263a403948ad7f48aa82
milos.peric <steamestt@gmail.com>
2020-06-22 16:00:42 +0200

    +Add fixex and comments

--- DAN_XXXVI_MilosPeric/Program.cs
+++ DAN_XXXVI_MilosPeric/Program.cs
@@ -12,6 +12,7 @@ namespace DAN_XXXVI_MilosPeric
         static void Main(string[] args)
         {
             MatrixFunctions treads = new MatrixFunctions();
+            //First part of the task starts here
             Thread t1 = new Thread(new ThreadStart(treads.GenerateMatrix));
             t1.Name = "Thread 1";
             Thread t2 = new Thread(new ThreadStart(treads.GenerateRandomNumbers));
@@ -20,6 +21,7 @@ namespace DAN_XXXVI_MilosPeric
             t2.Start();
             t1.Join();
             t2.Join();
+            //Second part of the task starts here
             Thread t3 = new Thread(new ThreadStart(treads.WriteOddNumbersToFile));
             t3.Name = "Thread 3";
             Thread t4 = new Thread(new ThreadStart(treads.ReadFile));
@@ -28,6 +30,7 @@ namespace DAN_XXXVI_MilosPeric
             t4.Start();
             t3.Join();
             t4.Join();
+            
             Console.ReadKey();
         }
     }
